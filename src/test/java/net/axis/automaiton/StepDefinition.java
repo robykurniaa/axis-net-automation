4e508bd528edcbf219245b6d671a7ae3db7d3a19
robykurniaa <robykurniaaa@gmail.com>
2022-07-08 10:56:10 +0700

    modified

--- src/test/java/net/axis/automaiton/StepDefinition.java
+++ src/test/java/net/axis/automaiton/StepDefinition.java
@@ -14,7 +14,7 @@ public class StepDefinition extends BaseTestClass {
     @Test(priority = 2, description = "Login ke Axis Net")
     public void login() {
         logger = reports.startTest("Testing login axis net");
-        loginPage.loginAxisNet("083114944146", "MANTAP");
+        loginPage.loginAxisNet("083114944147", "MANTAP");
         assertEquals(loginPage.getTextHome(), "Hai,");
     }
 }
